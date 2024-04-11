494edc97adb529acb4af087047fb331ee7735f93
Askhad Arzybaev <aarzybaev@gmail.com>
2024-04-11 23:22:20 +0600

    created AppToolbar component

---
+++ frontend/src/components/UI/AppToolbar/AppToolbar.tsx
@@ -0,0 +1,11 @@
+import {AppBar, Toolbar} from '@mui/material';
+
+const AppToolbar = () => {
+  return (
+    <AppBar position="sticky" sx={{mb: 2}}>
+      <Toolbar/>
+    </AppBar>
+  );
+};
+
+export default AppToolbar;
